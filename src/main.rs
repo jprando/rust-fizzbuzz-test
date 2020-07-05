896bd9d1efa7ac549d5d9eda3676410fd79e6183
Jeudi Prando Araujo <jeudi.araujo@el.com.br>
2020-07-05 19:07:59 -0300

    get value com IF

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
+fn get_value(num:i8) -> String {
+	let mut result = String::from("");
+
+	// match num {
+	// 	num if num % 3 == 0 => {
+
+	// 	},
+	// }
+
+	if num % 3 == 0 {
+		result = "FIZZ".to_string();
+	}
+
+	if num % 5 == 0 {
+		result += "BUZZ"
+	}
+
+	if result == "" {
+		result = num.to_string();
+	}
+
+	return result
+}
+
 fn main() {
-	for i in 1..100 {
-		println!("{}", i)
+	for i in 1..26 {
+		let value = get_value(i);
+		println!("{}", value);
 	}
 }
