bc9cc53cc90b8af1754d13b734030e603d9adb91
Jeudi Prando Araujo <jeudi.araujo@el.com.br>
2020-07-05 20:43:15 -0300

    get value match

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
 fn get_value(num: i8) -> String {
-	match num {
-		num if num % 15 == 0 => return "FIZZBUZZ".to_string(),
-		num if num % 5 == 0 => return "BUZZ".to_string(),
-		num if num % 3 == 0 => {
-			return "FIZZ".to_string();
-		}
-		_ => {
-			return num.to_string();
-		}
-	}
+	return match (num % 3, num % 5) {
+		(0, 0) => "FIZZBUZZ".to_string(),
+		(0, _) => "FIZZ".to_string(),
+		(_, 0) => "BUZZ".to_string(),
+		_ => num.to_string()
+	};
 }
 
 fn main() {
-	for i in 1..26 {
+	for i in 1..100 {
 		let value = get_value(i);
 		println!("{}", value);
-		// println!("{}", get_value(i));
 	}
 }
